bb2be378c0bce516745a07097d4b69b2947004a0
kalix70 <power.cs@hotmail.com.ar>
2021-01-27 17:59:44 -0300

    Recuperatorio

---
+++ README.md
@@ -0,0 +1,4 @@
+# e-commerce-Enero
+Proyecto de E-commerce recuperatorio Enero DH
+Nombre: Pablo Ezequiel Romero Muñoz
+fecha: Enero-Recuperatorio DH
---
+++ database/models/Categoria.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+
+    let cols = {
+        id:{
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    };
+
+    let config = {
+       tableName: "categorias",
+       timestamps: false,
+
+    };
+
+    const Categoria = sequelize.define("Categoria", cols, config);
+
+  
+
+    return Categoria;
+
+}
